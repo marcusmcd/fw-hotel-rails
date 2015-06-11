f5578e3e73aef9c9687810b425bea3ad2766820a
Marcus McDuffie <marcus.mcduffie@materne.us>
2015-06-10 23:44:29 -0400

    Made the Reservations model DRYer

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,17 +1,23 @@
 class Reservation < ActiveRecord::Base
+  after_initialize do |reservation|
+    @room = Room.find_by room_number: self.room_id
+  end
+  
   after_save :set_room_status
   before_destroy :check_room_status
   
+  room_numbers = Array (100..111) #Room numbers are 100 through 111 for this hotel.
+  
+  validates :room_id, inclusion: { in: room_numbers, message: "%{value} is not a valid room number" }
+  
   private
     def set_room_status
-      room = Room.find_by room_number: self.room_id
-      room.reserved = true
-      room.save
+      @room.reserved = true
+      @room.save
     end
     
     def check_room_status
-      room = Room.find_by room_number: self.room_id
-      room.reserved = false
-      room.save
+      @room.reserved = false
+      @room.save
     end
 end
