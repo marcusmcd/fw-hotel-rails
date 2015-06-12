83adc77070fdc2f1baaa09c692e65b5dcd8531c4
Marcus McDuffie <marcus.mcduffie@materne.us>
2015-06-12 09:45:03 -0400

    Setup some jQuery that resets the styles of reserved rooms.

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
   var today = $( "#hotel-date" ).text();
   $( ".hotel-room-table" ).before( "Date: <input type='text' id='hotel-room-date' value='" + today + "'></input>" );
-  $( "#hotel-room-date" ).datepicker();
+  $( "#hotel-room-date" ).datepicker({
+     onSelect: function(date) {
+       update_rooms(date);
+     }});
 });
+
+function update_rooms(date) {
+  $( "#hotel-date" ).text(date);
+  var res_cells = $( ".hotel-res-room-cell" );
+  res_cells.css("background-color", "#488689");
+}
