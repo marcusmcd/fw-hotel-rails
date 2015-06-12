dc7e03fe5fc549943a522a0152b742d0ad8c3b22
Marcus McDuffie <marcus.mcduffie@materne.us>
2015-06-12 09:06:01 -0400

    Added pre-filled date in correct format for JQuery datepicker.

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
-  $( ".hotel-room-table" ).before( "<input type='text' id='hotel-room-date'></input>" );
+  var today = $( "#hotel-date" ).text();
+  $( ".hotel-room-table" ).before( "Date: <input type='text' id='hotel-room-date' value='" + today + "'></input>" );
   $( "#hotel-room-date" ).datepicker();
 });
---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,2 @@
+#Needed this so that dates can display in the format needed by JQuery UI datepicker.
+Date::DATE_FORMATS[:month_day_year] = '%m/%d/%Y'
