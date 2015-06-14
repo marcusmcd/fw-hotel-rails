d129f4cd2e97994bcf5817823f2adb3a8cbba5ed
Marcus McDuffie <marcus.mcduffie@materne.us>
2015-06-14 01:03:42 -0400

    Got the JQuery working better, still not perfect.

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -11,4 +11,17 @@ function update_rooms(date) {
   $( "#hotel-date" ).text(date);
   var res_cells = $( ".hotel-res-room-cell" );
   res_cells.css("background-color", "#488689");
+  $( ".hotel-res-dates" ).each(function() {
+    var res_date = $(this).text();
+    var res_id   = $(this).data("resId");
+    if (res_date === date) {
+      var room_num = $(this).siblings().first().text();
+      $( ".hotel-res-room-cell" ).css("background-color", "#488689");
+      $( "#" + room_num ).css("background-color", "#864D57")
+        .text("")
+	.append("<a href='/reservations/" + res_id + "/edit'>" + room_num + "</a>");
+    } else {
+      $( ".hotel-res-room-cell" ).css("background-color", "#488689");
+    }
+  });
 }
